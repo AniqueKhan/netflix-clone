0c8ef3d19e287b9c392e48565e46d660f938ee78
AniqueKhan <aniquekhan004@gmail.com>
2021-04-24 01:28:29 +0500

    First commit

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Row from "./Row";
+
+function App() {
+  return (
+    <div className="App">
+      <Row title="Netflix" />
+      <Row title="Netflix" />
+      <Row title="Netflix" />
+      <Row title="Netflix" />
+      <Row title="Netflix" />
+    </div>
+  );
+}
+
+export default App;
